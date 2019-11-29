bd2ecab72d2f8a3f27df1e706c61588302afed10
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2019-11-29 23:22:59 +0800

    Adjust indent

--- frontend/index.php
+++ frontend/index.php
@@ -13,49 +13,49 @@
 ?>
 <!doctype html>
 <html>
-<head>
-	<title><?php echo $SITE_NAME; ?></title>
-	<link rel="stylesheet" type="text/css" href="panel.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="management.js?<?php echo time();?>"></script>
-	<script>
-		$(document).ready(() => {
-			init_management_page();
-		});
-	</script>
-</head>
-<body>
-	<h1 id="header_title">User mangent</h1>
-	
-	<div id="choose_panel">
-		<label>
-			<input type="radio" name="top_section_select_group" id="radio_user_management_panel" checked="checked">
-			User management
-		</label>
-		<label>
-			<input type="radio" name="top_section_select_group" id="radio_firebase_management_panel">
-			Firebase™ Cloud Messaging management
-		</label>
-	</div>
-
-	<hr>
-	<div>
-		<div id="div_user_management">
-			<button id="btn_add_user">Add user</button>
-			<button id="btn_refresh_user">Refresh</button>
+	<head>
+		<title><?php echo $SITE_NAME; ?></title>
+		<link rel="stylesheet" type="text/css" href="panel.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="management.js?<?php echo time();?>"></script>
+		<script>
+			$(document).ready(() => {
+				init_management_page();
+			});
+		</script>
+	</head>
+	<body>
+		<h1 id="header_title">User mangent</h1>
+		
+		<div id="choose_panel">
 			<label>
-				Search user:
-				<input type="text" id="txt_search_user_field">
+				<input type="radio" name="top_section_select_group" id="radio_user_management_panel" checked="checked">
+				User management
 			</label>
-			<br />
-			<div id="users_area"><!-- renender by javascript --></div>
-		</div> <!-- div_user_management -->
-
-		<div id="div_firebase_management" style="display: none;">
-			
+			<label>
+				<input type="radio" name="top_section_select_group" id="radio_firebase_management_panel">
+				Firebase™ Cloud Messaging management
+			</label>
 		</div>
-	</div>
-	<hr>
-	Click <a href="/login.php?action=logout">here</a> to logout Session.
-</body>
+
+		<hr>
+		<div>
+			<div id="div_user_management">
+				<button id="btn_add_user">Add user</button>
+				<button id="btn_refresh_user">Refresh</button>
+				<label>
+					Search user:
+					<input type="text" id="txt_search_user_field">
+				</label>
+				<br />
+				<div id="users_area"><!-- renender by javascript --></div>
+			</div> <!-- div_user_management -->
+
+			<div id="div_firebase_management" style="display: none;">
+				
+			</div>
+		</div>
+		<hr>
+		Click <a href="/login.php?action=logout">here</a> to logout Session.
+	</body>
 </html>
