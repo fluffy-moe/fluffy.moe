059b43ec9f2c23893c2a40368c001ad38ea2e8f5
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2019-12-10 23:17:49 +0800

    Android: Add capture photo activity

---
+++ android/mainpackage/src/main/java/moe/fluffy/app/CameraActivity.java
@@ -0,0 +1,13 @@
+package moe.fluffy.app.types;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
